10102a2f3cca0fdd12a54f86d4f01eb7e29e61dc
ezeltmann <eric.zeltmann@gmail.com>
2025-01-06 11:59:27 -0500

    added scaffold files

---
+++ player.py
@@ -0,0 +1,3 @@
+"""
+This class is to implement a player for the tetris piece so that we can use it to run simulations of what is happening.
+"""
---
+++ tetris_headless.py
@@ -0,0 +1,4 @@
+"""
+This is the implementation of a "headless" tetris, wherein the game can be played without showing the specific results.
+Specifically this will allow us to do all of the ai testing without actually having to watch the games play out.
+"""
